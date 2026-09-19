e7639381d4a67bcc7499bd7a6a8736d635a3efcb
qasim5259 <qasimkhan.1001@outlook.com>
2026-09-19 22:15:20 +0100

    Update settings and gitignore

--- apply_track/settings.py
+++ apply_track/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*7zo^)(4ts)n$1h+pa@-2wc3#c4q-5366(%0(d^**a+%#f$k4n'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
