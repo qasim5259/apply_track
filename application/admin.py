b1c38f3c9271d4a850f83dfed34dbdcb9f077d81
qasim5259 <qasimkhan.1001@outlook.com>
2026-09-20 20:23:57 +0100

    Add django-summernote to admin

--- application/admin.py
+++ application/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from .models import JobApplication
+from django_summernote.admin import SummernoteModelAdmin
 
 
 # Register your models here.
-admin.site.register(JobApplication)
+@admin.register(JobApplication)
+class JobApplicationAdmin(SummernoteModelAdmin):
+
+    list_display = ('company', 'job_title', 'status', 'application_date')
+    search_fields = ['company', 'job_title']
+    list_filter = ('status', 'application_date')
+    summernote_fields = ('notes',)
--- apply_track/urls.py
+++ apply_track/urls.py
@@ -19,7 +19,9 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path("", include("application.urls")),
     path('admin/', admin.site.urls),
+    path('summernote/', include('django_summernote.urls')),
+    path("", include("application.urls")),
+    
     
 ]
